3ae5300493c1ad941993d534badb98609211a194
ezequielcorrea14 <ezequielcorrea14.ec@gmail.com>
2023-11-16 17:30:30 -0300

    cambios

--- BC_Proyecto_Int_2_Ezequiel-Roberto-Correa/Parte 2/src/components/Background.jsx
+++ BC_Proyecto_Int_2_Ezequiel-Roberto-Correa/Parte 2/src/components/Background.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import video from './images/video2.mp4'
 
 const Background = () => {
   return (
     <div>
         <video onload="mostrarVideo()" id="videoFondo" muted autoplay loop>
-            <source id="mostrarVideo" src="../images/video/video2.mp4"/>
+            <source id="mostrarVideo" src={video}/>
         </video>
     </div>
   )
